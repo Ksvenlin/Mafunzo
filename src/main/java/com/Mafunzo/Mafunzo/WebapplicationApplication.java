9c0ef7299559a8c9446feb743abe8d59947f7395
Ksvenlin <svenlin.kasper@gmail.com>
2024-04-10 13:13:52 +0200

    Spring boot

---
+++ src/main/java/com/Mafunzo/Mafunzo/WebapplicationApplication.java
@@ -0,0 +1,14 @@
+package com.Mafunzo.Mafunzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class WebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapplicationApplication.class, args);
+	}
+
+}
